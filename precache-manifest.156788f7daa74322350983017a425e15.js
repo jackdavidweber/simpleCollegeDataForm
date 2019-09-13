44af88d17a3225eaacc76daf35bc091c519ea2ef
Jack Weber <jackdavidweber@gmail.com>
2019-09-12 21:00:39 -0700

    Updates

---
+++ precache-manifest.156788f7daa74322350983017a425e15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df197d57db6ab6a5f0316ffecf9640b7",
+    "url": "/simpleCollegeDataForm/index.html"
+  },
+  {
+    "revision": "781e962e16c6d7badab0",
+    "url": "/simpleCollegeDataForm/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "be61a5eb3cde0f85b8e4",
+    "url": "/simpleCollegeDataForm/static/js/2.ecc3bd0b.chunk.js"
+  },
+  {
+    "revision": "781e962e16c6d7badab0",
+    "url": "/simpleCollegeDataForm/static/js/main.549c5412.chunk.js"
+  },
+  {
+    "revision": "31e291611f6e62aab55d",
+    "url": "/simpleCollegeDataForm/static/js/runtime~main.a3624f10.js"
+  }
+]);
